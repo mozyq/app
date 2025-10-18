4b8facb45f9537ac11411f3fb152f95c534a8727
Gilad Kutiel <gilad.kutiel@gmail.com>
2025-10-18 03:07:58 +0300

    update

--- mozyq/mzq.py
+++ mozyq/mzq.py
@@ -158,7 +158,7 @@ def blocks(
 
 
 if __name__ == '__main__':
-    master = Path('./normalized/0469.jpg')
+    master = Path('./normalized/0519.jpg')
     tile_folder = Path('./blocks')
 
     gen_mzq_json(
